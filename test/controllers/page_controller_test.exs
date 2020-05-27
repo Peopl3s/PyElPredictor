2b8bb1a61514dc91e9ad3e1cb75bb8ec255f1a06
Peopl3s <nikposd@mail.ru>
2020-05-27 12:17:52 +0300

    Update page_controller_test.exs

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoenixMl.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Pleas"
+    assert html_response(conn, 200) =~ "Please"
   end
 end
